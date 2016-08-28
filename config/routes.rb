10a122e60ca2be23a809292045393f0f071bada7
babypero <maria.hirota@gmail.com>
2016-08-28 04:47:09 +0000

    Add 'Guess' resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'games#index'
 
+  resources :games do
+    resources :guesses, only: :create
+  end
   resources :games
 end
   # The priority is based upon order of creation: first created -> highest priority.
