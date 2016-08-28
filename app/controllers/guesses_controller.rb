968438096d88df63b0581ddd9239d2d4e69b8c64
babypero <maria.hirota@gmail.com>
2016-08-28 04:56:42 +0000

    Implement game logic

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,20 +1,25 @@
 class GuessesController < ApplicationController
   before_action :set_game
-  
+
   def create
-    @game.guesses.create!(guess_params)
-    
-    redirect_to game_path(@game), flash: {notice: 'Good guess!'}
+    guess = @game.guesses.create!(guess_params)
+
+    if guess.number == @game.number
+      @game.update(completed: true)
+      redirect_to game_path(@game), flash: { notice: 'Correct! You win!'}
+    else
+      redirect_to game_path(@game), flash: { notice: 'Nope. Try again.'}
+    end
   end
-  
+
   private
-  
+
   def set_game
     @game = Game.find(params[:game_id])
   end
-  
+
   def guess_params
     params.require(:guess).permit(:number)
   end
-  
+
 end
